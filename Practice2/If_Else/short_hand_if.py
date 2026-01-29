8e5826b2daf65c8be87da7104b64e6b961af8755
aiymalj08-gif <aiymalzhanbekova08@gmail.com>
2026-01-29 21:39:38 +0500

    Add Practice2 - Python control flow examples (if-else)

---
+++ Practice2/If_Else/short_hand_if.py
@@ -0,0 +1,3 @@
+a = 5
+b = 2
+if a > b: print("a is greater than b") #if the condition id false then it does not output anything 
---
+++ Practice2/If_Else/short_hand_if_else.py
@@ -0,0 +1,4 @@
+a = 2
+b = 330
+print("A") if a < b else print("B") # if the condition true it outputs A, if not it outputs B
+# this is called CONDITIONAL EXPRESSION
